a8371ced2df1d64a5c78ed03b102a2a7a47a9025
thawingx <thawingx@gmail.com>
2025-08-18 15:01:37 +0800

    fix：新增cors的header

--- supabase/functions/engagement-api-proxy/index.ts
+++ supabase/functions/engagement-api-proxy/index.ts
@@ -3,7 +3,7 @@ import { serve } from 'https://deno.land/std@0.168.0/http/server.ts'
 // CORS headers
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
+  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type, x-copilotcloud-public-api-key',
   'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, PUT, DELETE',
 }
 
